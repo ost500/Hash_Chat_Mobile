da0a21e034bcba7a986b2a13298bdbe58612560f
OST_MAC <ost5253@gmail.com>
2016-12-19 09:52:45 +0900

    chatting test

--- mytodos/www/js/chat.js
+++ mytodos/www/js/chat.js
@@ -1,5 +1,5 @@
 angular.module('mytodos.chat', [])
-    .directive('input', function($timeout) {
+    .directive('input', function ($timeout) {
         return {
             restrict: 'E',
             scope: {
@@ -8,26 +8,26 @@ angular.module('mytodos.chat', [])
                 'onFocus': '&',
                 'onBlur': '&'
             },
-            link: function(scope, element, attr) {
-                element.bind('focus', function(e) {
+            link: function (scope, element, attr) {
+                element.bind('focus', function (e) {
                     if (scope.onFocus) {
-                        $timeout(function() {
+                        $timeout(function () {
                             scope.onFocus();
                         });
                     }
                 });
-                element.bind('blur', function(e) {
+                element.bind('blur', function (e) {
                     if (scope.onBlur) {
-                        $timeout(function() {
+                        $timeout(function () {
                             scope.onBlur();
                         });
                     }
                 });
-                element.bind('keydown', function(e) {
+                element.bind('keydown', function (e) {
                     if (e.which == 13) {
-                        if (scope.returnClose) element[0].blur();
+                        // if (scope.returnClose) element[0].blur();
                         if (scope.onReturn) {
-                            $timeout(function() {
+                            $timeout(function () {
                                 scope.onReturn();
                             });
                         }
@@ -38,24 +38,45 @@ angular.module('mytodos.chat', [])
     })
 
 
-    .controller('ChatCtrl', function($scope, $timeout, $ionicScrollDelegate) {
+    .controller('ChatCtrl', function ($scope, $timeout, $ionicScrollDelegate) {
+
+        console.log('again');
+        var app = new BrainSocket(
+            new WebSocket('ws://192.168.10.10:8080'),
+            new BrainSocketPubSub()
+        );
+        var submitMessage = function () {
+            var name = $('#name').val();
+            var message = $('#message').val();
+            $('#message').val(''); // 폼 초기화
+            app.message('send.message', {name: name, message: message});
+        };
+        $('form').bind('submit', function () {
+            setTimeout(submitMessage, 0);
+            return false;
+        });
+
 
         $scope.hideTime = true;
 
         var alternate,
             isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
 
-        $scope.sendMessage = function() {
+        $scope.sendMessage = function () {
             alternate = !alternate;
 
             var d = new Date();
             d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
 
-            $scope.messages.push({
-                userId: alternate ? '12345' : '54321',
-                text: $scope.data.message,
-                time: d
-            });
+            // $scope.messages.push({
+            //     userId: alternate ? '12345' : '54321',
+            //     text: $scope.data.message,
+            //     time: d
+            // });
+
+            app.message('send.message',
+                {name: $scope.myId, message: $scope.data.message, hash_tag: $scope.hash_tag});
+
 
             delete $scope.data.message;
             $ionicScrollDelegate.scrollBottom(true);
@@ -63,20 +84,41 @@ angular.module('mytodos.chat', [])
         };
 
 
-        $scope.inputUp = function() {
+        // (3-3) 수신된 메시지 처리
+        app.Event.listen($scope.hash_tag, function (msg) {
+            console.log(msg.server.data.message);
+            alternate = !alternate;
+
+            var d = new Date();
+            d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
+
+            $scope.messages.push({
+                userId: msg.server.data.name,
+                text: msg.server.data.message,
+                time: d
+            });
+
+            $scope.$apply();
+
+            delete $scope.data.message;
+            $ionicScrollDelegate.scrollBottom(true);
+        });
+
+
+        $scope.inputUp = function () {
             if (isIOS) $scope.data.keyboardHeight = 216;
-            $timeout(function() {
+            $timeout(function () {
                 $ionicScrollDelegate.scrollBottom(true);
             }, 300);
 
         };
 
-        $scope.inputDown = function() {
+        $scope.inputDown = function () {
             if (isIOS) $scope.data.keyboardHeight = 0;
             $ionicScrollDelegate.resize();
         };
 
-        $scope.closeKeyboard = function() {
+        $scope.closeKeyboard = function () {
             // cordova.plugins.Keyboard.close();
         };
 
@@ -84,5 +126,6 @@ angular.module('mytodos.chat', [])
         $scope.data = {};
         $scope.myId = '12345';
         $scope.messages = [];
+        $scope.hash_tag = 'channel123';
 
     });
