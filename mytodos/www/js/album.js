52c4521b2dd153fdf2517d956ba59a6e96e412ec
sirwoongke <fzwoongbin@naver.com>
2017-03-01 17:34:05 +0900

    album create, list on-change

--- mytodos/www/js/album.js
+++ mytodos/www/js/album.js
@@ -1,6 +1,8 @@
 angular.module('mytodos.album', ['mytodos.list-data'])
-    .controller('AlbumCtrl', function ($scope, ListData, $stateParams, $location, $http) {
+    .controller('AlbumCtrl', function ($scope, ListData, $stateParams, $location, $http, $ionicNavBarDelegate) {
         console.log($location.url());
+
+        $ionicNavBarDelegate.showBackButton(false);
 
         $scope.posts = [];
 
@@ -53,11 +55,67 @@ angular.module('mytodos.album', ['mytodos.list-data'])
             console.log($stateParams.article_id);
             $location.path('/tab/album_detail/' + id);
         }
+
+        $scope.write = function () {
+            console.log("Create Click");
+            $location.path('/tab/album_create');
+        }
     })
-    .controller('AlbumDetailCtrl', function ($scope, $location, $stateParams, $http) {
+    .controller('AlbumDetailCtrl', function ($scope, $location, $stateParams, $http, $ionicNavBarDelegate) {
         $scope.post = "";
 
         $scope.moreDataCanBeLoaded = true;
+
+
+        $ionicNavBarDelegate.showBackButton(true);
+
+        function loadList(id, callback) {
+            $http.get('http://52.78.208.21/api/each_post/' + id)
+                .success(function (response) {
+                    var post;
+                    $scope.moreDataCanBeLoaded = false;
+                    post = response;
+                    callback(post);
+
+                    $scope.$broadcast('scroll.infiniteScrollComplete');
+                });
+        }
+
+        $scope.loadNew = function () {
+            $scope.moreDataCanBeLoaded = true;
+            loadList($stateParams.id, function (newData) {
+                $scope.post = newData;
+            });
+        };
+
+
+        $scope.loadMore = function () {
+            console.log('hi');
+            loadList($stateParams.id, function (data) {
+                $scope.post = data;
+                console.log(data);
+                $scope.$broadcast('scroll.infiniteScrollComplete');
+            });
+
+        };
+    })
+    .controller('AlbumCreateCtrl', function ($scope, $location, $stateParams, $http, LoginData, $ionicPopup) {
+        $scope.post = "";
+
+        $scope.moreDataCanBeLoaded = true;
+
+
+        var login_data = LoginData.get();
+        console.log(login_data);
+
+        $scope.create_data = {};
+
+
+        $scope.profile_data = {};
+
+        $scope.profile_data.name = login_data.name;
+        $scope.profile_data.email = login_data.email;
+        $scope.profile_data.profile_image = login_data.profile_image;
 
 
         function loadList(id, callback) {
@@ -90,41 +148,46 @@ angular.module('mytodos.album', ['mytodos.list-data'])
 
         };
 
-    })
-    .controller('AlbumCreateCtrl', function ($scope, $location, $stateParams, $http) {
-        $scope.post = "";
+        $scope.submit = function () {
 
-        $scope.moreDataCanBeLoaded = true;
+            console.log($scope.create_data);
 
+            $http({
+                method: 'POST',
+                url: 'http://52.78.208.21/api/posts/',
+                data: $.param({
+                    message: $scope.create_data.message,
+                    hashtag: $scope.create_data.hashtag
+                }),
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                }
+            }).success(function (response) {
+                console.log('response');
+                console.log(response);
+            }).error(function (response) {
+                console.log(response);
 
-        function loadList(id, callback) {
-            $http.get('http://52.78.208.21/api/each_post/' + id)
-                .success(function (response) {
-                    var post;
-                    $scope.moreDataCanBeLoaded = false;
-                    post = response;
-                    callback(post);
+                var error_message = "";
 
-                    $scope.$broadcast('scroll.infiniteScrollComplete');
+                if (response.message) {
+                    error_message = error_message + response.message;
+                }
+
+                if (response.hashtag) {
+                    if (response.message) {
+                        error_message = error_message + "<br>";
+                    }
+                    error_message = error_message + response.hashtag;
+                }
+
+                $ionicPopup.alert({
+                    title: "에러",
+                    template: error_message
                 });
+            });
+
+
         }
-
-        $scope.loadNew = function () {
-            $scope.moreDataCanBeLoaded = true;
-            loadList($stateParams.id, function (newData) {
-                $scope.post = newData;
-            });
-        };
-
-
-        $scope.loadMore = function () {
-            console.log('hi');
-            loadList($stateParams.id, function (data) {
-                $scope.post = data;
-                console.log(data);
-                $scope.$broadcast('scroll.infiniteScrollComplete');
-            });
-
-        };
 
     });
--- mytodos/www/js/list.js
+++ mytodos/www/js/list.js
@@ -2,31 +2,14 @@ angular.module('mytodos.list', ['mytodos.list-data'])
     .controller('ListCtrl', function ($http, $scope, ListData) {
 
 
-
-        // $scope.reorder = false;
-        //
-        // $scope.todos = TodoData.list();
-        //
-        // $scope.remove = function (todoId) {
-        //     TodoData.remove(todoId);
-        // };
-        //
-        // $scope.move = function (todo, fromIndex, toIndex) {
-        //     console.log(fromIndex + '/' + toIndex);
-        //     TodoData.move(todo, fromIndex, todoIndex);
-        // };
-        //
-        // $scope.toggleReorder = function () {
-        //     $scope.reorder = !$scope.reorder;
-        // };
         var tag = ListData.get_tag();
-        $http.get('http://52.78.208.21/api/hashtag?tag=수원대학교')
+        $scope.hashtags = "";
+
+        $http.get('http://52.78.208.21/api/hashtag?tag=' + ListData.get_tag())
             .success(function (response) {
                 console.log(response);
 
                 $scope.hashtags = response;
-                
-                
 
             });
 
@@ -39,46 +22,42 @@ angular.module('mytodos.list', ['mytodos.list-data'])
 
             });
 
-        //
-        // function loadList(callback) {
-        //     var tag = ListData.get_tag();
-        //     console.log('hi');
-        //     $http.get('/api/api/hash_tag_picture?tag=' + tag)
-        //         .success(function (response) {
-        //             var picture;
-        //
-        //             picture = response;
-        //
-        //             callback(picture);
-        //
-        //         });
-        // }
-        //
-        // $scope.loadNew = function () {
-        //
-        //     $scope.moreDataCanBeLoaded = true;
-        //
-        //     loadList(function (picture) {
-        //         $scope.picture = picture;
-        //     });
-        // };
-        // $scope.img_src = img.picture;
+
+        $scope.search = false;
+
+
+        $scope.search_text = {
+            text: ""
+        };
+
+
+
+        $scope.search_change = function () {
+
+            console.log($scope.search_text.text);
+
+            if ($scope.search_text.text == "") {
+                $scope.search = false;
+
+                $http.get('http://52.78.208.21/api/hashtag?tag=' + ListData.get_tag())
+                    .success(function (response) {
+                        console.log(response);
+
+                        $scope.hashtags = response;
+
+                    });
+
+                return;
+            }
+
+            $scope.search = true;
+
+            $http.get('http://52.78.208.21/api/hashtag?tag=' + $scope.search_text.text)
+                .success(function (response) {
+                    console.log(response);
+
+                    $scope.hashtags = response;
+                });
+        }
+
     });
-// .controller('ListCtrl', function ($scope, TodoData) {
-//     $scope.reorder = false;
-//
-//     $scope.todos = TodoData.list();
-//
-//     $scope.remove = function (todoId) {
-//         TodoData.remove(todoId);
-//     };
-//
-//     $scope.move = function (todo, fromIndex, toIndex) {
-//         console.log(fromIndex + '/' + toIndex);
-//         TodoData.move(todo, fromIndex, todoIndex);
-//     };
-//
-//     $scope.toggleReorder = function () {
-//         $scope.reorder = !$scope.reorder;
-//     };
-// })
